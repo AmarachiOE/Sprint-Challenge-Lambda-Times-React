d3469b532bc9cccab32a1480c7c8a9fea9ff4fa3
AmarachiOE <ao.ejiawoko@gmail.com>
2019-03-29 08:38:12 -0700

    Tabs are rendered on screen

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -4,10 +4,22 @@ const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+
+  // create empty tab classname variable
+  // can't use const because it's read only
+  let tabClassName = "";
+
+  // add conditional value to that variable
+  if (props.tab === props.selectedTab) {
+    tabClassName = 'tab active-tab'
+  } else {
+     tabClassName = 'tab'
+  }
+
   return (
     <div
-      className={''}
-      onClick={() => {
+      className={tabClassName}
+      onClick={() => { props.selectTabHandler(props.tab)
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
       }}
